d0440b0fdc961ed109197c247b56c4840700e40a
darshvallur <darshvallur@gmail.com>
2021-04-30 08:50:24 +0530

    css overlay fixes

--- react-portals/src/Modal.js
+++ react-portals/src/Modal.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 const MODAL_STYLES = {
-  postion: 'fixed',
+  position: 'fixed',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   backgroundColor: '#FFF',
   padding: '50px',
-  zIndex: 1000,
-};
+  zIndex: 1000
+}
 
 const OVERLAY_STYLES = {
-  postion: 'fixed',
-  left: '0',
-  right: '0',
-  bottom: '0',
-  backgroundColor: 'rgba(0,0,0, .7)',
-  zIndex: 1000,
-};
-
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, .7)',
+  zIndex: 1000
+}
 const Modal = ({ open, children, onClose }) => {
   if (!open) return null;
 
